264e332ac2448d20fc551ad16b5be99da514e5c6
Pratyush Kumar <Pratyush Kumar@SMTC-R-0172>
2023-03-31 13:14:28 +0530

    testing for url(smoke Testing)

---
+++ testing_ecoders/src/varify_url/Testing_contact_url.java
@@ -0,0 +1,23 @@
+package varify_url;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testing_contact_url
+{
+public static void main(String[] args) throws Throwable {
+	ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("http://127.0.0.1:5000/contact");
+	Thread.sleep(2000);
+	String url=driver.getCurrentUrl();
+	System.out.println(url);
+	String expturl="http://127.0.0.1:5000/contact";
+	if(expturl.equals(url)) {
+		System.out.println("testcase is passed");
+	}
+	else {
+		System.out.println("test case is failed");
+	}
+	driver.quit();
+}
+}
